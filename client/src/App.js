ad47827dfbc77d7365308b99e6092e2c135a3b69
Eoin Whelan <whelan.eoin6@gmail.com>
2022-07-27 00:11:35 -0700

    Add penguin image + reinstate certificate file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './penguin.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
